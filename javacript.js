69bfc68c4122af4f9b0a41a333e7f8fb60b17e09
tuyenlddev <tuyenld.dev@gmail.com>
2023-03-30 22:27:57 +0700

    main

--- javacript.js
+++ javacript.js
@@ -1,1 +1,28 @@
 console.log('hello 500 ae')
+
+let x = 2, y = 2, c=  3;
+
+console.log('x= ',x,'y= ',y,'c= ',c);
+
+let text = `asdsdsada"sd"asdasdasd  `;
+
+console.log(text.length);
+console.log(text);
+
+
+// loại bỏ khoảng trắng
+console.log(text.trim().length);
+
+// Phương thức của string
+
+let a = `2.3.4.5.6.7`
+
+console.log(a.split('.'))
+
+let xa = 10, b = 20, d = '1';
+let e = Number(d);
+
+console.log(b===e, 'b + ', typeof b);
+// ngoài toán tử cộng, các toán tử khác sẽ được chuyển đổi cho phù hợp có giá trị
+// không phân biệt số nguyên, số thực
+
---
+++ learning.md
@@ -0,0 +1,31 @@
+<!-- _Khai báo biến_ -->
+let a = '10';
+console.log(a)
+
+<!-- _khai báo hằng số_ -->
+const a = 10;
+. biến không bao giờ thay đổi
+
+<!-- _Các kiểu dữ liệu_ (data types) -->
+
+_string_ : chuỗi 'là chuỗi ký tự' được đặt trong dấu '';
+
+_number_ : giá trị số;
+
+_Booleans_: giá trị True or False
+
+_typeof_: 
+
+_undefined_: 
+
+<!-- Giá trị Number -->
+
+_NaN_: not a number
+
+_Number_: số
+
+<!-- chuỗi -->
+
+_string.length_: độ dài của chuỗi
+
+_string methods_
